9730ad13f1d7c6c5080320e5f6043539499fd19f
Brent Schneeman <schneeman@gmail.com>
2020-05-25 16:47:59 -0500

    comments, and increasing the palette for lots of states (which is a little ridiculous)

--- states-ranking.R
+++ states-ranking.R
@@ -1,64 +1,83 @@
+library(tidyverse)
+library(RcppRoll)
+library(hrbrthemes)
+library(ggthemes)
 
 covid_state <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
 
+# I thought I was going to be able to use the daily's to do some factors, (fct_lump_n), but
+# turns out that some states have negative daily numbers (corrections to the accounting?) 
+# so nope
+covid_state <- covid_state %>% 
+  group_by(fips) %>% 
+  arrange(date) %>%
+  mutate(daily_cases = replace_na(cases - lag(cases),0),
+         daily_deaths = replace_na(deaths - lag(deaths),0))%>%
+  ungroup()
 
-covid_sl <- covid_state %>% 
-  mutate(lumped_d = fct_lump_n(state, n=8, w=deaths)) %>%
-  group_by(lumped_d, date) %>%
+# the last row is the current (today() - 1 day) tally, so find the
+# top n
+
+top_count <- 10
+
+top_by_deaths_tib = covid_state %>% filter(date == max(date)) %>%
+  top_n(top_count, wt = deaths) %>% arrange(desc(deaths))
+
+top_by_cases_tib = covid_state %>% filter(date == max(date)) %>%
+  top_n(top_count, wt = cases) %>% arrange(desc(cases))
+
+# and now create factors so that we can plot easier. Create a new level
+# that will accumulate all of the "others"
+
+covid_state <- covid_state %>% 
+  mutate(top_by_deaths = if_else(state %in% top_by_deaths_tib$state, state, "Other"),
+         top_by_cases = if_else(state %in% top_by_cases_tib$state, state, "Other")) %>%
+  mutate(top_by_deaths = factor(top_by_deaths, levels = c(top_by_deaths_tib$state, "Other")),
+         top_by_cases = factor(top_by_cases, levels = c(top_by_cases_tib$state, "Other")))
+
+# and now start accumulating by the factors
+covid_sl <- covid_state %>%
+  group_by(top_by_deaths, date) %>%
   summarize(deaths = sum(deaths)) %>%
   ungroup() 
 
-state_levels <- covid_sl %>% filter(lumped_d != "Other") %>% 
-  group_by(lumped_d) %>%
-  tally(deaths) %>% 
-  mutate(lumped_d = fct_reorder(lumped_d, -n))
-
-state_levels <- fct_expand(state_levels$lumped_d, "Other")
-
-covid_sl$lumped_d = factor(covid_sl$lumped_d, levels(state_levels))
-
+# calculate a 7-day moving average (a rolling mean)
 covid_sla <- covid_sl %>% 
-  group_by(lumped_d) %>%
+  group_by(top_by_deaths) %>%
   arrange(date) %>%
   mutate(delta_d = deaths - lag(deaths),
          mean_7 = roll_mean(delta_d, 7, fill=0, align="right"))
 
+my_pal <- c(ipsum_pal()(9), few_pal()(8), colorblind_pal()(8))
+
+# and plot the deaths
 covid_sla %>%
-  ggplot(aes(x=date, y=mean_7, fill=lumped_d)) + geom_area() +
+  ggplot(aes(x=date, y=mean_7, fill=top_by_deaths)) + geom_area() +
   theme_ipsum() +
-  scale_fill_ipsum("States") +
+  scale_fill_manual("States", values = my_pal) +
   labs(
     title = "COVID-19 New Deaths",
     subtitle = "7 day moving average",
     y = "Count"
   )
 
-
-covid_sl <- covid_state %>% 
-  mutate(lumped_d = fct_lump_n(state, n=8, w=cases)) %>%
-  group_by(lumped_d, date) %>%
+# do everything that we did above for deaths, but for cases
+covid_sl <- covid_state %>%
+  group_by(top_by_cases, date) %>%
   summarize(cases = sum(cases)) %>%
   ungroup() 
 
-state_levels <- covid_sl %>% filter(lumped_d != "Other") %>% 
-  group_by(lumped_d) %>%
-  tally(cases) %>% 
-  mutate(lumped_d = fct_reorder(lumped_d, -n))
-
-state_levels <- fct_expand(state_levels$lumped_d, "Other")
-
-covid_sl$lumped_d = factor(covid_sl$lumped_d, levels(state_levels))
-
 covid_sla <- covid_sl %>% 
-  group_by(lumped_d) %>%
+  group_by(top_by_cases) %>%
   arrange(date) %>%
-  mutate(delta_i = cases - lag(cases),
-         mean_7 = roll_mean(delta_i, 7, fill=0, align="right"))
+  mutate(delta_c = cases - lag(cases),
+         mean_7 = roll_mean(delta_c, 7, fill=0, align="right"))
+
 
 covid_sla %>%
-  ggplot(aes(x=date, y=mean_7, fill=lumped_d)) + geom_area() +
+  ggplot(aes(x=date, y=mean_7, fill=top_by_cases)) + geom_area() +
   theme_ipsum() +
-  scale_fill_ipsum("States") +
+  scale_fill_manual("States", values = my_pal) +
   labs(
     title = "COVID-19 New Cases",
     subtitle = "7 day moving average",
