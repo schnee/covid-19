f4047257517bf0b99a182389dd195b454c9a5e06
Brent Schneeman <schneeman@gmail.com>
2020-06-25 12:44:59 -0500

    using data in the package

---
+++ covidutil/data-raw/covid_pal---c-hrbrthemes-ipsum_pal-9-ggthemes-few_pal-8-ggthemes-colorblind_pal-8-.R
@@ -0,0 +1,3 @@
+## code to prepare `covid_pal <- c(hrbrthemes::ipsum_pal()(9), ggthemes::few_pal()(8), ggthemes::colorblind_pal()(8))` dataset goes here
+
+usethis::use_data(covid_pal <- c(hrbrthemes::ipsum_pal()(9), ggthemes::few_pal()(8), ggthemes::colorblind_pal()(8)), overwrite = TRUE)
